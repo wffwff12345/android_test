22ecc14aee72ac38e13fe623cb7861064dbc7168
wucp <2696982657@qq.com>
2022-11-18 10:00:34 +0900

    完善学习用例

--- mylistview/src/main/java/com/example/mylistview/MainActivity.java
+++ mylistview/src/main/java/com/example/mylistview/MainActivity.java
@@ -3,12 +3,33 @@ package com.example.mylistview;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
+import android.widget.AdapterView;
+import android.widget.ListView;
+
+import java.util.ArrayList;
+import java.util.List;
 
 public class MainActivity extends AppCompatActivity {
 
+    private static final String TAG = "leo";
+    private List<Bean> data = new ArrayList<>();
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-    }
-}
+        for (int i = 0; i < 100; i++) {
+            Bean bean = new Bean();
+            bean.setName("test"+i);
+            data.add(bean);
+        }
+        ListView view = findViewById(R.id.lv);
+        view.setAdapter(new MyAdapter(data,this));
+        view.setOnItemClickListener(new AdapterView.OnItemClickListener() {
+            @Override
+            public void onItemClick(AdapterView<?> parent, View view, int position, long id) {
+                Log.e(TAG, "onItemClick: "+position );
+            }
+        });
+}}
